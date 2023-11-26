0c5e7fba48774cd9fb1f4b7ea529d6af79e76636
Miguelito2926 <juniorprol@hotmail.com>
2023-11-26 19:54:55 -0300

    Documentando e versionando a Api

--- ApiCatalogoRepositoryAssincrono/obj/Debug/net8.0/ApiCatalogoRepositoryAssincrono.AssemblyInfo.cs
+++ ApiCatalogoRepositoryAssincrono/obj/Debug/net8.0/ApiCatalogoRepositoryAssincrono.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiCatalogoRepositoryAssincrono")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc971bcf6720f76d8faaac0a60ec2d436fc5e356")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiCatalogoRepositoryAssincrono")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiCatalogoRepositoryAssincrono")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
